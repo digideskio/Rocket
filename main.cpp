20c7762bd1d55a8e71bcc190040f2ad47e5b9e80
unknown <silversquirrel88@gmail.com>
2015-10-04 20:21:55 -0500

    Zoomed out, increased number of rockets from 1 to 16

--- main.cpp
+++ main.cpp
@@ -60,15 +60,36 @@ void DisplayFunc()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(50.0, width / double(height), 1.0, 10.0);
-	glViewport(0, 0, width, height);
+	gluPerspective(100.0, width / double(height), 1.0, 100.0);
+	glViewport(0, 10, width, height);
 	glEnable(GL_DEPTH_TEST);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt(0.0, 0.0, 4.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	glTranslated(0, 0, -5);
-	Spaceship s1;
-	s1.draw();
+	glTranslated(0.0, 0.0, -5.0);
+	glPushMatrix();
+	glTranslated(-15.0, -5.0, 0.0);
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			//glTranslated(7.5, 0.0, 0.0);
+			Spaceship s1;
+			s1.draw();
+			glTranslated(7.5, 0.0, 0.0);
+		}
+		if (i % 2 == 0)
+		{
+			glTranslated(0.0, 0.0, -7.5);
+			glRotated(180.0, 0.0, 1.0, 0.0);
+		}
+		else
+		{
+			glRotated(180.0, 0.0, 1.0, 0.0);
+			glTranslated(0.0, 0.0, -7.5);
+		}
+	}
+	glPopMatrix();
 	glutSwapBuffers();
 }
 
